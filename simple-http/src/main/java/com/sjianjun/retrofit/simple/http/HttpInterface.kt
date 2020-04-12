1469751098b99c6dd1c6142c6c660d32fb706eda
shen <978622005@qq.com>
2020-04-12 20:49:11 +0800

    encoded = true

--- simple-http/src/main/java/com/sjianjun/retrofit/simple/http/HttpInterface.kt
+++ simple-http/src/main/java/com/sjianjun/retrofit/simple/http/HttpInterface.kt
@@ -6,11 +6,11 @@ import retrofit2.http.*
 interface HttpInterface {
 
     @GET
-    fun get(@Url url: String, @QueryMap queryMap: Map<String, String> = emptyMap(), @HeaderMap header: Map<String, String> = emptyMap()): Deferred<String>
+    fun get(@Url url: String, @QueryMap(encoded = true) queryMap: Map<String, String> = emptyMap(), @HeaderMap header: Map<String, String> = emptyMap()): Deferred<String>
 
 
     @FormUrlEncoded
     @POST
-    fun post(@Url url: String = "", @FieldMap fieldMap: Map<String, String> = emptyMap(), @HeaderMap header: Map<String, String> = emptyMap()): Deferred<String>
+    fun post(@Url url: String = "", @FieldMap(encoded = true) fieldMap: Map<String, String> = emptyMap(), @HeaderMap header: Map<String, String> = emptyMap()): Deferred<String>
 
 }
