46a25983aaf0306a221c42f708d66839ba0bc328
shen <978622005@qq.com>
2021-07-18 01:06:21 +0800

    jitpack.io

--- app/src/main/java/com/sjianjun/retrofit/simplify/JavaTest.java
+++ app/src/main/java/com/sjianjun/retrofit/simplify/JavaTest.java
@@ -4,7 +4,7 @@ import com.sjianjun.retrofit.simple.http.HttpClient;
 
 public class JavaTest {
     public static void test() {
-        HttpClient.create().get(String.class,"https://www.biquge5200.cc/95_95192/");
-        HttpClient.create().post(String.class,"https://www.biquge5200.cc/95_95192/");
+        HttpClient.create().get(String.class,"https://baijiahao.baidu.com/builder/theme/bjh/login");
+        HttpClient.create().post(String.class,"https://baijiahao.baidu.com/builder/theme/bjh/login");
     }
 }
